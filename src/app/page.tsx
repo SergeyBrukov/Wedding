46cc01523c60481b722f519832b17a15fe08a458
Sergey Brykov <s.brykov@vegangster.com>
2025-03-09 15:32:48 +0200

    fix slug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
 import React, { Suspense } from "react";
-import About from "@/components/section/about/About";
 import Gallery from "@/components/section/gallery/Gallery";
 import Schedule from "@/components/section/schedule/Schedule";
 import Contact from "@/components/section/contact/Contact";
 import galleryService from "@/service/Gallery.service";
-import aboutService from "@/service/About.service";
 
-const getAboutOurStory = async () => {
-  const data = await aboutService.API_METHODS.getAbout();
-  return data;
-};
-
-export default async function Home() {
+export default  function Home() {
   const FOLDER_ID = "1MOJtgldHMpqkObPqvqNU6OTDWLP5pALK";
-  const aboutData = await fetch('/api/about-section')
-
-  console.log("aboutData>>SKA", aboutData);
 
   return (
     <div>
 
-      <About />
 
       {/* Галерея */}
       <Suspense fallback={<p className="text-red-500 py-16 bg-mochaMousse-100">Gallery loading....</p>}>
