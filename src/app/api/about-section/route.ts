b00a3f1e505b8667f907ec88296bfef6cad01d1a
Sergey Brykov <s.brykov@vegangster.com>
2025-03-09 15:09:15 +0200

    fix slug

--- src/app/api/about-section/route.ts
+++ src/app/api/about-section/route.ts
@@ -1,8 +1,7 @@
-import { NextResponse } from "next/server";
-import aboutData from "../../../../public/data/about.json";
+import { NextResponse } from 'next/server';
+import aboutData from '../../../../public/data/about.json';
 
-export async function GET () {
-  return NextResponse.json({
-    ...aboutData,
-  });
+export async function GET() {
+  console.log('About data loaded:', aboutData); // Логування даних
+  return NextResponse.json(aboutData);
 }
