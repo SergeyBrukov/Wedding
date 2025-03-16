d0caa4fb3257b2dbc8b6ac2e6582fdd4aeea685a
Sergey Brykov <s.brykov@vegangster.com>
2025-03-16 10:19:00 +0200

    fix scrolling

--- src/app/scrollPage/ScrollPage.tsx
+++ src/app/scrollPage/ScrollPage.tsx
@@ -7,49 +7,51 @@ const ScrollPage = () => {
   const sectionIds = ["about", "schedule", "gallery", "contact"];
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: "instant",
-    });
+    if (typeof window !== "undefined" && "scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
+    const handleScroll = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "instant",
+      });
+    };
+
+    handleScroll();
+
   }, [pathname]);
 
   useEffect(() => {
     const intersectionObserver = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         const target = entry.target as HTMLElement;
-        if (!sectionIds.some(item => item === target.id)) {
-          return;
-        }
+        if (!sectionIds.includes(target.id)) return;
+
         if (entry.isIntersecting && entry.intersectionRatio >= 0.3) {
           console.log("Intersecting", entry.target);
-          target.classList.add("animate-opacity-in");
+          target.classList.add("animate-opacity-in", "opacity-1");
           target.classList.remove("opacity-0");
-          target.classList.add("opacity-1");
           observer.unobserve(target);
         }
       });
-    }, {
-      threshold: [0, 0.3, 0.5, 0.7, 1],
-    });
-
+    }, { threshold: [0, 0.3, 0.5, 0.7, 1] });
 
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => {
-      if (sectionIds.some(item => item === section.id)) {
+      if (sectionIds.includes(section.id)) {
         intersectionObserver.observe(section);
       }
     });
 
     return () => {
       sections.forEach((section) => {
-        if (sectionIds.some(item => item === section.id)) {
-          intersectionObserver.observe(section);
-        }
+        intersectionObserver.unobserve(section);
       });
     };
-  }, [pathname]);
+  }, []);
 
-  return <></>;
+  return null;
 };
 
 export default memo(ScrollPage);
